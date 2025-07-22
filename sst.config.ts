cefcdbfd597aa2806f8f159317f0e1214ba34225
Emil Kristoffersen <>
2025-07-22 17:51:07 +0200

    changed sst config

--- sst.config.ts
+++ sst.config.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./.sst/platform/config.d.ts" />
 
@@ -14,3 +15,29 @@ export default $config({
     new sst.aws.Nextjs("MyWeb");
   },
 });
+=======
+import { type SSTConfig } from "sst";
+import { NextjsSite } from "sst/constructs";
+
+export default {
+  config(_input) {
+    return {
+      name: "t3-sst",
+      region: "us-east-1",
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const site = new NextjsSite(stack, "site", {
+        environment: {
+          DATABASE_URL: process.env.DATABASE_URL!,
+        },
+      });
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;
+>>>>>>> ab74c84 (changed sst config)
