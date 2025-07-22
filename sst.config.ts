b3eb541e4a9c2fe9315cccf51d58b2c8009be7f3
Emil Kristoffersen <>
2025-07-22 17:58:40 +0200

    changed sst config v2

--- sst.config.ts
+++ sst.config.ts
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="./.sst/platform/config.d.ts" />
-
-export default $config({
-  app(input) {
-    return {
-      name: "winetracker",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      protect: ["production"].includes(input?.stage),
-      home: "aws",
-    };
-  },
-  async run() {
-    new sst.aws.Nextjs("MyWeb");
-  },
-});
-=======
 import { type SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
 
@@ -40,4 +22,3 @@ export default {
     });
   },
 } satisfies SSTConfig;
->>>>>>> ab74c84 (changed sst config)
