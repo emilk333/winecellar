43390d2b483608bba5ceb1b4586a7868c9d70d97
Emil Kristoffersen <>
2025-07-25 14:10:53 +0200

    WHHHOOOOOOOOOOOOPS

--- sst.config.ts
+++ sst.config.ts
@@ -14,9 +14,9 @@ export default $config({
 		// Create a Next.js site
 		new sst.aws.Nextjs("winetrackerFrontend", {
 			environment: {
-				DATABASE_URL:"postgresql://postgres.etrtnmlfyidtbnjboden:Shitfaced_333@aws-0-eu-west-3.pooler.supabase.com:6543/postgres",
-        NEXT_PUBLIC_SUPABASE_URL:"https://etrtnmlfyidtbnjboden.supabase.co",
-        NEXT_PUBLIC_SUPABASE_ANON_KEY:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0cnRubWxmeWlkdGJuamJvZGVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczMTMyMDksImV4cCI6MjA1Mjg4OTIwOX0.2lrzHFVWjNlAtwJGvaRsKfg_hvmEI3fHDifRfo_1n5Q"
+				DATABASE_URL: process.env.DATABASE_URL! || "",
+				NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL! || "",
+				NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! || ""
 			},
 		})
 	},
